0cf9aa68a95990f0aa6829d85e32ef833e6b64b4
Elok Mutiaraningtyas <elokmutiaraningtyas@gmail.com>
2026-02-22 03:23:36 +0700

    Add exercises for creating and manipulating 2D NumPy arrays

--- numpy.py
+++ numpy.py
@@ -101,4 +101,223 @@ print(np_height_in[100:111])
 # --------------------------------------------------------------------------------------------------------
 
 
+# Your First 2D NumPy Array
+# Before working on the actual MLB data, let's try to create a 2D numpy array from a small list of lists.
+
+# In this exercise, baseball is a list of lists. The main list contains 4 elements. Each of these elements is a list containing the height and the weight of 4 baseball players, in this order. baseball is already coded for you in the script.
+
+# Instructions
+# 100 XP
+# Use np.array() to create a 2D numpy array from baseball. Name it np_baseball.
+
+# Print out the type of np_baseball.
+# Print out the shape attribute of np_baseball. Use np_baseball.shape.
+
+
+import numpy as np
+
+baseball = [[180, 78.4],
+            [215, 102.7],
+            [210, 98.5],
+            [188, 75.2]]
+
+# Create a 2D numpy array from baseball: np_baseball
+np_baseball = np.array(baseball)
+
+# Print out the type of np_baseball
+print(type(np_baseball))
+
+# Print out the shape of np_baseball
+print(np_baseball.shape)
+
+# output : 
+# <class 'numpy.ndarray'>
+# (4, 2)
+
+# --------------------------------------------------------------------------------------------------------
+
+# Baseball data in 2D form
+# You realize that it makes more sense to restructure all this information in a 2D numpy array.
+
+# You have a Python list of lists. In this list of lists, each sublist represents the height and weight of a single baseball player. The name of this list is baseball and it has been loaded for you already (although you can't see it).
+
+# Store the data as a 2D array to unlock numpy's extra functionality.
+
+# Instructions
+# 100 XP
+# Use np.array() to create a 2D numpy array from baseball. Name it np_baseball.
+# Print out the shape attribute of np_baseball
+
+
+import numpy as np
+
+# Create a 2D numpy array from baseball: np_baseball
+np_baseball = np.array(baseball)
+
+# Print out the shape of np_baseball
+
+print(np_baseball.shape)
+
+# output:  (1015, 2)
+
+
+
+# ----------------------------------------------------------------------------------------------------------------------
+
+# Subsetting 2D NumPy Arrays
+# If your 2D numpy array has a regular structure, i.e. each row and column has a fixed number of values, complicated ways of subsetting become very easy. Have a look at the code below where the elements "a" and "c" are extracted from a list of lists.
+
+# # numpy
+# import numpy as np
+# np_x = np.array(x)
+# np_x[:, 0]
+# The indexes before the comma refer to the rows, while those after the comma refer to the columns. The : is for slicing; in this example, it tells Python to include all rows.
+
+# Instructions
+# 100 XP
+# Print out the 50th row of np_baseball.
+# Make a new variable, np_weight_lb, containing the entire second column of np_baseball.
+# Select the height (first column) of the 124th baseball player in np_baseball and print it out.
+
+import numpy as np
+
+np_baseball = np.array(baseball)
+
+# Print out the 50th row of np_baseball
+print(np_baseball[49, :])
+
+# Select the entire second column of np_baseball: np_weight_lb
+np_weight_lb = np_baseball[:, 1]
+
+
+# Print out height of 124th player
+print(np_baseball[123, 0])
+
+
+#output: 
+
+# [ 70 195]
+# 75
+
+
+# --------------------------------------------------------------------------------------------------------------------
+
+
+# 2D Arithmetic
+# 2D numpy arrays can perform calculations element by element, like numpy arrays.
+
+# np_baseball is coded for you; it's again a 2D numpy array with 3 columns representing height (in inches), weight (in pounds) and age (in years). baseball is available as a regular list of lists and updated is available as 2D numpy array.
+
+# Instructions
+# 100 XP
+# You managed to get hold of the changes in height, weight and age of all baseball players. It is available as a 2D numpy array, updated. Add np_baseball and updated and print out the result.
+# You want to convert the units of height and weight to metric (meters and kilograms, respectively). As a first step, create a numpy array with three values: 0.0254, 0.453592 and 1. Name this array conversion.
+# Multiply np_baseball with conversion and print out the result.
+
+import numpy as np
+
+np_baseball = np.array(baseball)
+
+# Print out addition of np_baseball and updated
+print(np_baseball + updated)
+
+# Create numpy array: conversion
+conversion = np.array([0.0254, 0.453592, 1])
+
+# Print out product of np_baseball and conversion
+print(np_baseball * conversion)
+
+
+# output :
+
+# [[ 75.2303559  168.83775102  23.99      ]
+#  [ 75.02614252 231.09732309  35.69      ]
+#  [ 73.1544228  215.08167641  31.78      ]
+#  ...
+#  [ 76.09349925 209.23890778  26.19      ]
+#  [ 75.82285669 172.21799965  32.01      ]
+#  [ 73.99484223 203.14402711  28.92      ]]
+# [[ 1.8796  81.64656 22.99   ]
+#  [ 1.8796  97.52228 34.69   ]
+#  [ 1.8288  95.25432 30.78   ]
+#  ...
+#  [ 1.905   92.98636 25.19   ]
+#  [ 1.905   86.18248 31.01   ]
+#  [ 1.8542  88.45044 27.92   ]]
+
+
+# --------------------------------------------------------------------------------------------------------------------
+
+
+# Basic Statistic
+
+# Average versus median
+# You now know how to use numpy functions to get a better feeling for your data.
+
+# The baseball data is available as a 2D numpy array with 3 columns (height, weight, age) and 1015 rows. The name of this numpy array is np_baseball. After restructuring the data, however, you notice that some height values are abnormally high. Follow the instructions and discover which summary statistic is best suited if you're dealing with so-called outliers. np_baseball is available.
+
+# Instructions
+# 100 XP
+# Create numpy array np_height_in that is equal to first column of np_baseball.
+# Print out the mean of np_height_in.
+# Print out the median of np_height_in.
+
+import numpy as np
+
+# Create np_height_in from np_baseball
+np_height_in = np_baseball[:, 0]
+
+# Print out the mean of np_height_in
+print(np.mean(np_height_in))
+
+# Print out the median of np_height_in
+print(np.median(np_height_in))
+
+# **Output:**
+# ```
+# 1586.46
+# 74.0
+
+
+# --------------------------------------------------------------------------------------------------------------------
+
+
+# Explore the baseball data
+# Because the mean and median are so far apart, you decide to complain to the MLB. They find the error and send the corrected data over to you. It's again available as a 2D NumPy array np_baseball, with three columns.
+
+# The Python script in the editor already includes code to print out informative messages with the different summary statistics and numpy is already loaded as np. Can you finish the job? np_baseball is available.
+
+# Instructions
+# 100 XP
+# The code to print out the mean height is already included. Complete the code for the median height.
+# Use np.std() on the first column of np_baseball to calculate stddev.
+# Do big players tend to be heavier? Use np.corrcoef() to store the correlation between the first and second column of np_baseball in corr
+
+
+avg = np.mean(np_baseball[:,0])
+print("Average: " + str(avg))
+
+# Print median height
+med = np.median(np_baseball[:,0])
+print("Median: " + str(med))
+
+# Print out the standard deviation on height
+stddev = np.std(np_baseball[:,0])
+print("Standard Deviation: " + str(stddev))
+
+# Print out correlation between first and second column
+corr = np.corrcoef(np_baseball[:,0], np_baseball[:,1])
+print("Correlation: " + str(corr))
+
+# output :
+
+# Average: 73.6896551724138
+# Median: 74.0
+# Standard Deviation: 2.312791881046546
+# Correlation: [[1.         0.53153932]
+#  [0.53153932 1.        ]]
+
+
+
+
 
